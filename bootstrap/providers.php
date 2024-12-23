3baf9ba7a837edfd41cc0f14e7cbae297e753a69
Jake Richhart <7827558+doubleoh13@users.noreply.github.com>
2024-12-22 19:54:37 -0500

    Add Laravel Horizon integration to the project
    Integrated Laravel Horizon for queue monitoring by adding its configuration, provider, and dependencies. Updated `composer.json`, registered `HorizonServiceProvider`, and included Horizon settings in `config`.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,6 +5,7 @@ return [
     App\Providers\EventServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\GoogleServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\NotionServiceProvider::class,
     App\Providers\TodoistServiceProvider::class,
 ];
