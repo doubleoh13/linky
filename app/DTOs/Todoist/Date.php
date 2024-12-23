62e3bef4911d802a64298bb0ae50d477e6d850b2
Jake Richhart <7827558+doubleoh13@users.noreply.github.com>
2024-12-22 21:05:20 -0500

    Refactor Date DTO to use string type for date field
    Replaced CarbonImmutable type with string for the $date property in the Date DTO. This simplifies the object structure and removes the dependency on DateTime casting in this class.

--- app/DTOs/Todoist/Date.php
+++ app/DTOs/Todoist/Date.php
@@ -2,16 +2,12 @@
 
 namespace App\DTOs\Todoist;
 
-use Carbon\CarbonImmutable;
-use Spatie\LaravelData\Attributes\WithCast;
-use Spatie\LaravelData\Casts\DateTimeInterfaceCast;
 use Spatie\LaravelData\Data;
 
 class Date extends Data
 {
     public function __construct(
-        #[WithCast(DateTimeInterfaceCast::class)]
-        public CarbonImmutable $date,
+        public string $date,
         public bool $is_recurring,
         public string $lang,
         public string $string,
